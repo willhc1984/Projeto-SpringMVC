f7e8ebcb0ff4660c2f1d0464024641bf77d6529a
will henrique <willhc1984@gmail.com>
2022-01-26 14:18:02 -0300

    Inicio do projeto

---
+++ src/main/java/com/whcampos/projetomvc/ProjetoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.whcampos.projetomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whcampos/projetomvc/ProjetoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whcampos.projetomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
